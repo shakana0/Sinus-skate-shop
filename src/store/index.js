1b222cb34abdd06e70fc5149f254c25dbcda8f7d
idaniths <idan.cheaproot@iths.se>
2022-02-22 14:43:21 +0100

    Saved data in state

--- src/store/index.js
+++ src/store/index.js
@@ -6,20 +6,23 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state:{
-    products :{}
+    products: []
   },
   mutations: {
     saveItems(state, items){
-      // state.products.push(items);
-      console.log(items)
-      // console.log(state.products);
+      state.products.push(items);
     }
   },
   actions: {
     async getItems(context, query){
-      console.log(query);
-      const response = await api.getItems(query);
-      context.commit("saveItems", response.data);
+      // Om products i state har en kategori som är samma som query 
+      // vars längd på värde är mindre än 1 (är tom/finns ej). Om den inte finns gör vi API-anropet.
+      if(context.state.products.filter(category => category[query]).length < 1){
+        const response = await api.getItems(query);
+        context.commit("saveItems", {[query]: response.data.products});
+      } else {
+        console.log("hej")
+      }
     }
   },
   modules: {},
