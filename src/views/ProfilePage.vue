fbd08cc10485878da6713d4e49d1812c80507f57
Shakana Jamal <shakana.jamal.moalin@iths.se>
2022-03-02 10:30:09 +0100

    added styling

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,57 +1,152 @@
 <template>
   <main>
-    <p>{{getUserData}}</p>
-    <article v-for="order in getOrders" :key="order.id">
-      <h2>{{order.status}}</h2>
-      <p>{{order.shippingCity}}</p>
-      <p>{{order.shippingStreet}}</p>
-      <p>{{order.shippingZip}}</p>
-      <p>{{order.id}}</p>
-      <div v-for="item in order.items" :key="item.id">
-        <p>id: {{item.id}}</p>
-        <p>price: {{item.price}}</p>
-        <p>Amount: {{item.amount}}</p>
+    <h2>YOUR INFORMATION</h2>
+<section>
+  <div class="info">
+        <p>Name:</p>
+        <p>{{ getUserData.name }}</p>
       </div>
-    </article>
+      <div class="info">
+        <p>Email</p>
+        <p>{{ getUserData.email }}</p>
+      </div>
+       <div class="info">
+        <p>City</p>
+        <p>{{ getUserData.address.city }}</p>
+      </div>
+       <div class="info">
+        <p>Street</p>
+        <p>{{ getUserData.address.street }}</p>
+      </div>
+       <div class="info">
+        <p>Zip</p>
+        <p>{{ getUserData.address.zip }}</p>
+      </div>
+</section>
+  <h2>Order history</h2>
+    <article v-for="order in getOrders" :key="order.id">
+       <div class="info">
+        <p>Created At:</p>
+        <p>{{ order.createdAt }}</p>
+      </div>
+      <div class="info">
+        <p>Order Status:</p>
+        <p>{{ order.status }}</p>
+      </div>
+       <div>
+          <p>Ordernumber:</p>
+          <p>{{ order.id }}</p>
+        </div>
     
-    <button @click="logout" >Log out</button>
+      <section v-for="item in order.items" :key="item.id">
+        <div class="line"></div>
+        <div>
+          <p>Article Number:</p>
+          <p>{{ item.ProductId }}</p>
+        </div>
+        <div class="line"></div>
+        <div>
+          <p>Price:</p>
+          <p>{{ item.price }}</p>
+        </div>
+        <div class="line"></div>
+        <div>
+          <p>Amount:</p>
+          <p>{{ item.amount }}</p>
+        </div>
+      </section>
+    </article>
+
+    <button @click="logout">LOG OUT</button>
   </main>
 </template>
 
 <script>
 export default {
-  computed:{
-    getUserData(){
-      return this.$store.state.user.name;
+  computed: {
+    getUserData() {
+      return this.$store.state.user;
     },
-    getOrders(){
-      //let orders = this.$store.state.orders;
-      // if (orders == null) {
-      //   return "";
-      // }
-      // //console.log(orders)
+    getOrders() {
       return this.$store.state.orders;
-    }
+    },
   },
-  methods:{
-    logout(){
-      this.$router.push({path: '/'})
+  methods: {
+    logout() {
+      this.$router.push({ path: "/" });
       this.$store.dispatch("logout");
-    }
+    },
   },
-   created(){
-     this.$store.dispatch('getOrders');
-   },
+  created() {
+    this.$store.dispatch("getOrders");
+  },
   // updated(){
   //   this.$store.dispatch('getOrders');
   // }
-  
+
   // updated(){
   //   this.$store.dispatch('getOrders');
   // }
-}
+};
 </script>
 
 <style scoped lang="scss">
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 
+  button {
+    background-color: #df0000;
+    text-align: center;
+    font-size: 1.4rem;
+    cursor: pointer;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    padding: 1rem 2rem;
+    margin-bottom: 2rem;
+  }
+  button:hover {
+    background-color: #e18c8c;
+  }
+}
+article {
+  width: 400px;
+  h2 {
+    margin-bottom: 2rem;
+  }
+
+  .info {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+    border: 1px solid black;
+    padding: 1rem;
+
+    p:nth-child(2) {
+      font-weight: 600;
+    }
+  }
+
+  section {
+    margin-bottom: 2rem;
+    .line {
+      width: 100%;
+      height: 1px;
+      background-color: #cdcdcd;
+      padding: 0;
+      border: none;
+    }
+    div {
+      display: flex;
+      justify-content: space-between;
+
+      p {
+        padding: 1rem;
+      }
+    }
+  }
+}
 </style>
